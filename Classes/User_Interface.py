f136c1ff56b84925e4190c563be56c73115d523b
Joel <ohmaj4699@gmail.com>
2018-01-10 09:25:45 -0600

    transition from horizontal to vertical menu options

--- Classes/User_Interface.py
+++ Classes/User_Interface.py
@@ -16,8 +16,8 @@ class UserInterface:
 
     def main_menu(self):
         self.cls()
-        print('Main Menu \n ------------ \n [1]Golden Eagle [2] Ariens [3] Kawasaki [4] AIP [5] CPD [6] Oscar Wilson '
-              '[7] Ideal [8] Run All [exit] Exit Program')
+        print('Main Menu \n ------------ \n [1]Golden Eagle \n[2] Ariens \n[3] Kawasaki \n[4] AIP \n[5] CPD \n'
+              '[6] Oscar Wilson \n[7] Ideal \n[8] Run All \n[exit] Exit Program')
         user_selection = input('What would you like to do?: ')
         if user_selection == '1':
             self.golden_eagle_menu()
@@ -35,8 +35,7 @@ class UserInterface:
             ideal = Ideal_Scrape.DatabaseConnection()
             ideal.write_inventory()
         elif user_selection == '8':
-            run_all = Run_All.RunAll()
-            run_all.run()
+            self.run_all()
         elif user_selection == 'exit':
             sys.exit()
         else:
@@ -45,8 +44,8 @@ class UserInterface:
         self.main_menu()
 
     def run_all(self):
-        print('Main Menu \n ------------ \n [1] All Web Scrapes [2] Non-Scrape All [3] All All [back] Back to Main Menu'
-              ' [exit] Exit Program')
+        print('Main Menu \n ------------ \n[1] All Web Scrapes \n[2] Non-Scrape All \n[3] All All '
+              '\n[back] Back to Main Menu \n[exit] Exit Program')
         user_selection = input('What would you like to do?: ')
         if user_selection == '1':
             run_all = Run_All_web.RunAll()
@@ -68,7 +67,7 @@ class UserInterface:
 
     def aip_menu(self):
         self.cls()
-        print('Main Menu \n ------------ \n [1] Quantity Scrape [back] Back to Main Menu [exit] Exit Program')
+        print('Main Menu \n ------------ \n[1] Quantity Scrape \n[back] Back to Main Menu \n[exit] Exit Program')
         user_selection = input('What would you like to do?: ')
         if user_selection == '1':
             aip = Distributor_AIP.AIP('AIP')
@@ -85,8 +84,8 @@ class UserInterface:
 
     def cpd_menu(self):
         self.cls()
-        print('CPD Menu \n ------------ \n [1] XML Inquiry Quantity [2] Get XML Response Only '
-              '[back] Back to Main Menu [exit] Exit Program')
+        print('CPD Menu \n ------------ \n[1] XML Inquiry Quantity \n[2] Get XML Response Only '
+              '\n[back] Back to Main Menu \n[exit] Exit Program')
         user_selection = input('What would you like to do?: ')
         if user_selection == '1':
             self.cpd_xml_menu()
@@ -104,8 +103,8 @@ class UserInterface:
 
     def ow_menu(self):
         self.cls()
-        print('Oscar Wilson Menu \n ------------ \n [1] MTD [2] Maruyama [3] AYP [back] Back to Main Menu '
-              '[exit] Exit Program')
+        print('Oscar Wilson Menu \n ------------ \n[1] MTD \n[2] Maruyama \n[3] AYP \n[back] Back to Main Menu '
+              '\n[exit] Exit Program')
         user_selection = input('What would you like to do?: ')
         if user_selection == '1':
             mtd = From_File_OscarWilson.UpdateInventory('MTD')
@@ -128,7 +127,7 @@ class UserInterface:
 
     def kawasaki_menu(self):
         self.cls()
-        print('CPD Scrape Menu \n ------------ \n [1] Scrape Kawasaki [back] Back to Main Menu [exit] Exit Program')
+        print('CPD Scrape Menu \n ------------ \n[1] Scrape Kawasaki \n[back] Back to Main Menu \n[exit] Exit Program')
         user_selection = input('What would you like to do?: ')
         if user_selection == '1':
             kaw = Distributor_Kawasaki.Kawasaki('KAW')
@@ -145,7 +144,7 @@ class UserInterface:
 
     def ariens_menu(self):
         self.cls()
-        print('CPD Scrape Menu \n ------------ \n [1] Scrape Ariens [back] Back to Main Menu [exit] Exit Program')
+        print('CPD Scrape Menu \n ------------ \n[1] Scrape Ariens \n[back] Back to Main Menu \n[exit] Exit Program')
         user_selection = input('What would you like to do?: ')
         if user_selection == '1':
             ariens = Distributor_Ariens.Ariens('ARN')
@@ -162,7 +161,7 @@ class UserInterface:
 
     def golden_eagle_menu(self):
         self.cls()
-        print('CPD Scrape Menu \n ------------ \n [1] Scrape Echo [2] Scrape Billygoat [3] Scrape Both '
+        print('CPD Scrape Menu \n ------------ \n[1] Scrape Echo \n[2] Scrape Billygoat \n[3] Scrape Both '
               '[back] Back to Main Menu [exit] Exit Program')
         user_selection = input('What would you like to do?: ')
         if user_selection == '1':
@@ -187,8 +186,8 @@ class UserInterface:
 
     def cpd_xml_response(self):
         self.cls()
-        print('CPD XML Response Menu \n ------------ \n [1] Kohler [2] Tecumseh [3] Hydro Gear [4] Case [5] AYP [6]All '
-              '[back] Back to CPD Menu [exit] Exit Program')
+        print('CPD XML Response Menu \n ------------ \n[1] Kohler \n[2] Tecumseh \n[3] Hydro Gear \n[4] Case '
+              '\n[5] AYP [6]All \n[back] Back to CPD Menu \n[exit] Exit Program')
         user_selection = input('What would you like to do?: ')
         if user_selection == '1':
             koh = XML_CPD.CPD('KOH')
@@ -242,8 +241,8 @@ class UserInterface:
 
     def cpd_xml_menu(self):
         self.cls()
-        print('CPD XML Quantity Menu \n ------------ \n [1] Kohler [2] Tecumseh [3] Hydro Gear [4] Case [5] AYP '
-              '[6] Martin Wheels [7] All [back] Back to CPD Menu [exit] Exit Program')
+        print('CPD XML Quantity Menu \n ------------ \n[1] Kohler \n[2] Tecumseh \n[3] Hydro Gear \n[4] Case \n[5] AYP '
+              '\n[6] Martin Wheels \n[7] All \n[back] Back to CPD Menu \n[exit] Exit Program')
         user_selection = input('What would you like to do?: ')
         if user_selection == '1':
             koh = XML_CPD.CPD('KOH')
