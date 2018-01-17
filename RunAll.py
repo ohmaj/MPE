f41abcd3413140ba8942a2b55b37eee02d801de1
Joel <ohmaj4699@gmail.com>
2018-01-17 14:14:35 -0600

    both run all's

---
+++ RunAll.py
@@ -0,0 +1,12 @@
+from Classes import Run_All_web
+from Classes import Run_All_Not_Web
+
+
+def start():
+    run_all_scrapes = Run_All_Not_Web.RunAll()
+    run_all_scrapes.run()
+    run_all_scrapes = Run_All_web.RunAll()
+    run_all_scrapes.run()
+
+
+start()
